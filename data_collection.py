b3fe73a0b51c95b9fac74ec7fc91c31cc694f8ac
Forest <zhou_forest@yahoo.com>
2021-05-28 16:30:21 -0400

    get confirmed cases

--- data_collection.py
+++ data_collection.py
@@ -20,16 +20,16 @@ def state_index():
     return s_index
 
 
-def cases(case_type, time_frame='delta', days_ago=0, index='All'):
+def case_day(case_type, time_frame='delta', days_ago=0, index='All'):
     """
-    gets number of cases
+    gets number of case on a given day
     :param case_type: confirmed, recovered, deceased
     :param time_frame: delta for selected day, delta7 for last 7 days, total for cumulative
     :param days_ago: data from x number of days ago
     :param index: specify province (int, str)
     :return: dictionary of province(s) and cas count(s)
     """
-    date = datetime.datetime.now() - datetime.timedelta(days=days_ago)
+    date = datetime.datetime.now() - datetime.timedelta(days=days_ago+1)
     date = date.strftime("%Y-%m-%d")
     results = {}
 
@@ -57,5 +57,43 @@ def cases(case_type, time_frame='delta', days_ago=0, index='All'):
             return results
 
 
+def cases_list(case_type, length, index='All'):
+    """
+    gets list of historical data
+    :param case_type: confirmed, recovered, deceased
+    :param length: quantity of historical data (days)
+    :param index: specify province (int, str)
+    :return: dictionary with state code and list of case numbers
+    """
+    results = {}
+    cases = []
+
+    if type(index) == int:
+        index = state_index()[index]
+
+    if index == 'All':
+        for state in raw_json:
+            for key in raw_json[state]['dates']:
+                try:
+                    cases.append(raw_json[state]['dates'][key]['delta'][case_type])
+                except KeyError:
+                    cases.append('n/a')
+
+            results[state] = cases[-length:]
+            cases = []
+        return results
+
+    else:
+        for key in raw_json[index]['dates']:
+            try:
+                cases.append(raw_json[index]['dates'][key]['delta'][case_type])
+            except KeyError:
+                cases.append('n/a')
+
+        results[index] = cases[-length:]
+        return results
+
+
 if __name__ == '__main__':
-    print(cases('confirmed', time_frame='delta7', index=1))
+    # print(case_day('confirmed', time_frame='delta7', index=1))
+    print(cases_list('recovered', 5, 'AP'))
